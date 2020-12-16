20627e689eec6121c6a3d326ba51db0d0c7c18f0
jose antonio <rodrigues.jantonio84@gmail.com>
2020-12-16 00:16:15 -0300

    criacao modulo de rotas

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer,CursoSerializer
 
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """ Exibir todos alunos """
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 # Create your views here.
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """ Exibir todos Cursos """
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from escola.views import AlunosViewSet, CursosViewSet
+
+
+router = routers.DefaultRouter()
+router.register('alunos',AlunosViewSet,basename='Alunos')
+router.register('cursos',CursosViewSet,basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
